f739568759ddc6debe63e32fb9ab142940a10972
Sam Gates <sam.gates@heliosinteractive.com>
2020-05-25 11:11:41 -0400

    better parameter handling

--- src/restful/api.clj
+++ src/restful/api.clj
@@ -4,14 +4,14 @@
             [clojure.pprint :as pp]
             [clojure.data.json :as json]
             [clojure.string :as str]
-            [restful.client :as client]))
+            [restful.client :as client]
+            [clojure.set :refer [rename-keys]]))
 
 (def people-collection (atom []))
 
 (defn add-person [firstname surname]
-  (swap! people-collection conj {:firstname (str/capitalize firstname) :surname (str/capitalize surname)}))
-
-(defn get-parameters [req pname] (get (:params req) pname))
+  (swap! people-collection conj {:firstname (str/capitalize firstname)
+                                 :surname (str/capitalize surname)}))
 
 (defn simple-body-page [_]
   {:status 200
@@ -34,7 +34,7 @@
 (defn add-person-handler [req]
   {:status 200
    :headers {"Content-Type" "text/json"}
-   :body (-> (let [p (partial get-parameters req)]
+   :body (-> (let [p (:params req)]
                (str (json/write-str (add-person (p :firstname) (p :surname))))))})
 
 (defn people-handler [_]
@@ -42,19 +42,29 @@
    :headers {"Content-Type" "text/json"}
    :body (str (json/write-str @people-collection))})
 
+(defn data-transducer [data]
+  (let [key-map {:title  :book/title
+                 :author :book/author
+                 :genre  :book/genre}]
+    (->> (filter (fn [[key val]]
+                   (contains? (set (keys key-map)) key)) data)
+         (map (fn [[key val]]
+                [(key-map key) val]))
+         (into {}))))
+
 (defn add-book-handler [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (-> (let [b (partial get-parameters req)]
-               (client/put {:book/title  (b :title)
-                            :book/author (b :author)
-                            :book/genre  (b :genre)})
-               (client/all-books)))})
+   :body  (let [b (:params req)]
+            (when (not-empty (filter some? b))
+              (client/put (data-transducer b)))
+            (client/all-books))})
+
 
 (defn books-handler [_]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body  (client/all-books)})
+   :body  (client/all-unique-titles)})
 
 (c/defroutes app-routes
   (c/GET "/" [] simple-body-page)
--- src/restful/client.clj
+++ src/restful/client.clj
@@ -9,7 +9,7 @@
 
 (def client (d/client config))
 
-(def conn (d/connect client {:db-name "hello"}))
+(def conn (d/connect client {:db-name "books"}))
 
 (defn db [] (d/db conn))
 
@@ -28,13 +28,23 @@
                    :db/cardinality :db.cardinality/one
                    :db/doc         "The genre of the book"}])
 
-(defonce schema (d/transact conn {:tx-data book-schema}))
+(def schema (d/transact conn {:tx-data book-schema}))
 
 (defn put [data] (d/transact conn {:tx-data [data]}))
 
 (def all-books-q '[:find ?title ?author ?genre
                    :where [?e :book/title  ?title]
-                          [?e :book/author ?author]
-                          [?e :book/genre  ?genre]])
+                   [?e :book/author ?author]
+                   [?e :book/genre  ?genre]])
 
-(defn all-books [] (d/q all-books-q (db)))
+(def all-books-t '[:find (pull ?e [*])
+                   :where [?e :book/title  ?title]])
+
+(defn all-books [] (d/q all-books-t (db)))
+
+(defn all-unique-titles []
+  (let [books (d/q all-books-t (db))]
+    (-> (fn [[entry]] 
+          (dissoc entry :db/id))
+        (map books)
+        (set))))
